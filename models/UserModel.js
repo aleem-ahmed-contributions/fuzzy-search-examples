512f1256d224fbcfa978a008beda0fb3a2a1df12
aleem-ahmed <aleem.ahmed2072@gmail.com>
2020-12-17 21:06:13 -0500

    Better Query

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,17 +1,48 @@
 // [REQUIRE] //
-const { mongoose } = require('../connect')
 const mongoose_fuzzy_searching = require('mongoose-fuzzy-searching')
 
 
+// [REQUIRE] Personal // 
+const { mongoose } = require('../connect')
+
+
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  age: Number,
+	firstName: String,
+
+	lastName: String,
+
+	email: String,
+
+	bio: String,
+
+	age: Number,
 }).plugin(
-  mongoose_fuzzy_searching,
-  { fields: ['firstName', 'lastName', 'email']
-})
+	mongoose_fuzzy_searching,
+	{
+		fields: [
+			{
+				name: 'firstName',
+				minSize: 2,
+				weight: 5,
+			},
+			{
+				name: 'lastName',
+				minSize: 2,
+				weight: 5,
+			},
+			{
+				name: 'email',
+				minSize: 2,
+				weight: 5,
+			},
+			{
+				name: 'bio',
+				minSize: 5,
+				weight: 5,
+			},
+		]
+	}
+)
 
 
 // [EXPORT] //
--- search/mongoose-fuzzy-search.js
+++ search/mongoose-fuzzy-search.js
@@ -3,15 +3,48 @@ const UserModel = require('../models/UserModel')
 
 const mfs = async () => {
 	try {
-		await new UserModel({
-			firstName: 'Joe',
-			lastName: 'Doe',
-			email: 'joe.doe@mail.com',
+		// Save
+		const saved = await new UserModel({
+			firstName: 'joe',
+			lastName: 'doe',
+			email: 'brosho@example.com',
+			bio: 'i love ice cream',
 			age: 30
 		}).save()
 	
-		const searchResult = await UserModel.fuzzySearch('jo')
-		console.log('search result', searchResult)
+
+		if (saved) {
+			/**
+			 * Below are sample queries. these should output data.
+			 */
+	
+	
+			// query 1
+			const q1 = await UserModel.fuzzySearch({
+				query: 'brosho',
+			})
+			console.log('q1 search result:', q1)
+	
+	
+			// query 2
+			const q2 = await UserModel.fuzzySearch({
+				query: 'love',
+			})
+			console.log('q2 search result:', q2)
+	
+			// query 3
+			const q3 = await UserModel.fuzzySearch({
+				query: 'i love',
+			})
+			console.log('q3 search result:', q3)
+	
+			// query 4
+			const q4 = await UserModel.fuzzySearch({
+				query: 'i cream',
+			})
+			console.log('q4 search result:', q4)
+		}
+
 	}
 	catch (e) { console.error(e) }
 }
